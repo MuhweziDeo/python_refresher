0d100f88524ff780f1cee8afabfee1025c648f8b
muhwezideo <aggrey256@gmail.com>
2020-03-19 00:17:22 +0300

    strings and variables

---
+++ strings.py
@@ -0,0 +1,11 @@
+x = 'deo'
+y = 'me'
+
+# concatenate
+print('Hello' + x + "iam" + y)
+
+# Argument position
+print('my name is {name}'.format(name=x))
+
+# F string
+print(f'{x}')
---
+++ variables.py
@@ -0,0 +1,11 @@
+x = 2
+print(x)
+
+# multiple assignment
+a, b, c, d = (1, 2, 5, 9)
+
+print(a, b, c, d)
+
+print(type(str(a)))
+
+
